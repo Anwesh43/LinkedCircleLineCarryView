076b960b739d4d8af207818b291fdb2f6f5f3ba0
Anwesh43 <anweshthecool0@gmail.com>
2021-04-03 01:56:43 +0530

    adding constants and libraries

--- circlelinecarryview/src/main/java/com/example/circlelinecarryview/CircleLineCarryView.kt
+++ circlelinecarryview/src/main/java/com/example/circlelinecarryview/CircleLineCarryView.kt
@@ -1,2 +1,26 @@
 package com.example.circlelinecarryview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#2196F3",
+    "#E65100",
+    "#64DD17",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
