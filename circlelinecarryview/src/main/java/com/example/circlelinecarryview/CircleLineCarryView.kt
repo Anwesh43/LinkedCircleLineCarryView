f601923f228da000409f1a6df5cd2086015a3d8c
Anwesh43 <anweshthecool0@gmail.com>
2021-04-03 02:23:50 +0530

    adding CLCNode class

--- circlelinecarryview/src/main/java/com/example/circlelinecarryview/CircleLineCarryView.kt
+++ circlelinecarryview/src/main/java/com/example/circlelinecarryview/CircleLineCarryView.kt
@@ -117,4 +117,45 @@ class CircleLineCarryView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CLCNode(var i : Int, val state : State = State()) {
+
+        private var next : CLCNode? = null
+        private var prev : CLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCLCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CLCNode {
+            var curr : CLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
